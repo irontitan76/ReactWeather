ffbf08885497c981a89f05117666a71205a08aed
Sheppard <ross_sheppard@outlook.com>
2016-06-08 22:45:50 -0500

    Changed site title

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -18,7 +18,7 @@ var Nav = React.createClass({
       <div className="top-bar">
         <div className="top-bar-left">
           <ul className="menu">
-            <li className="menu-text">Skyline Technologies</li>
+            <li className="menu-text">React Weather App</li>
             <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Weather</IndexLink></li>
             <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
             <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
